d69f644740163df70c36387b4d22ef603f1f65e7
r3dskhie <wilf.pineda@yahoo.com>
2016-10-03 20:38:58 +0800

    Debugged - Need testing

--- Hoenn Safari.lua
+++ Hoenn Safari.lua
@@ -548,14 +548,14 @@ function natureSwap()
 	
 end
 function sleepPP()
-	if sleepmove ~= "None" then 
+	if sleepmove == "Hypnosis" or sleepmove == "Spore" or sleepmove == "Sleep Powder" or sleepmove ~= "None" then 
 		return getRemainingPowerPoints(sleeper, sleepmove) >= 1 
 	elseif sleepmove == "None" then
 		isPokemonUsable(sleeper)
 	end
 end
 function fswipePP()
-	if weakmove ~= "None" then 
+	if weakmove == "False Swipe" or weakmove ~= "None" then 
 		return getRemainingPowerPoints(falseswiper, weakmove) >= 1 
 	elseif weakmove == "None" then
 		isPokemonUsable(falseswiper)
