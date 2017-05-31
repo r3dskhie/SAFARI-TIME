5e4ea91367e8fdf597908dbf0a2642ec7e223d57
r3dskhie <wilf.pineda@yahoo.com>
2017-05-31 06:57:54 +0200

    Add files via upload

---
+++ Safari/SafariMaps.lua
@@ -0,0 +1,35 @@
+function AreaOne()
+	map = getMapName()
+	if map == "Safari Area 1" then
+		moveToGrass()
+	else
+		PathFinder.moveTo(map, "Safari Area 1")
+	end
+end
+
+function AreaTwo()
+	map = getMapName()
+	if map == "Safari Area 2" then
+		moveToGrass()
+	else
+		PathFinder.moveTo(map, "Safari Area 2")
+	end
+end
+
+function AreaThree()
+	map = getMapName()
+	if map == "Safari Area 3" then
+		moveToGrass()
+	else
+		PathFinder.moveTo(map, "Safari Area 3")
+	end
+end
+
+function Exclusive()
+	map = getMapName()
+	if map == "Safari Exclusive" then
+		moveToGrass()
+	else
+		PathFinder.moveTo(map, "Safari Exclusive")
+	end
+end
