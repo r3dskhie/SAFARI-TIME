34fd9d14c969e76478265fd0a567c43eca4a2f6b
r3dskhie <wilf.pineda@yahoo.com>
2017-06-09 10:36:43 +0800

    Add files via upload

---
+++ Safari/PokeConditions.lua
@@ -0,0 +1,48 @@
+local falseswiper = nil
+local sleeper = nil
+local synchro = nil
+local sleepMoves = {"Sleep Powder", "Hypnosis", "Spore", "Yawn", "Sing"}
+
+function getPokemonNumberWithMove(move)
+    for i = 1, getTeamSize() do
+        if hasMove(i, move) then
+            return i
+        end
+    end
+    return nil
+end
+
+function getPokemonNumberOfSynch(Nat)
+    for i = 1, getTeamSize() do
+        if getPokemonNature(i, Nat) then
+            return i
+        end
+    end
+    return nil
+end
+
+if multipleCatch == true then
+	return synchro == getPokemonNumberOfSynch("Synchronize")
+end
+
+if advanceCatchOn == true then
+	
+	return falseswiper == getPokemonNumberWithMove("False Swipe") and sleeper == getPokemonNumberWithMove(sleepMoves)
+
+	
+elseif fswipeOn == true then
+
+	return falseswiper == getPokemonNumberWithMove("False Swipe")
+	
+	
+elseif sleepOnOn == true then
+	
+	return sleeper == getPokemonNumberWithMove(sleepMoves)
+	
+	
+end
+
+for _,sleepMoves in pairs(sleepMoves) do
+  sleeper = sleepMoves
+    break
+end
