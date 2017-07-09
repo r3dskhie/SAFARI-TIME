2f44b87cede9285baec9736aad2d2666d05f6a92
r3dskhie <wilf.pineda@yahoo.com>
2017-07-09 13:04:29 +0800

    Sleep and False Swipe conditions

--- Safari/PokeConditions.lua
+++ Safari/PokeConditions.lua
@@ -1,49 +1,20 @@
-local falseswiper = nil
-local sleeper = nil
-local synchro = nil
+
 local sleepMoves = {"Sleep Powder", "Hypnosis", "Spore", "Yawn", "Sing"}
-
-function getPokemonNumberWithMove(move)
-    for i = 1, getTeamSize() do
-        if hasMove(i, move) then
-            return i
+if falseswiper == nil then
+for pokemonId=1,getTeamSize(),1 do
+    for pokemonId=1, getTeamSize(), 1 do
+        if hasMove(pokemonId, "False Swipe") then
+            falseswiper = pokemonId
         end
     end
-    return nil
 end
-
-function getPokemonNumberOfSynch(Nat)
-    for i = 1, getTeamSize() do
-        if getPokemonNature(i, Nat) then
-            return i
+end
+if sleeper == nil then
+for pokemonId=1,getTeamSize(),1 do
+    for pokemonId=1, getTeamSize(), 1 do
+        if hasMove(pokemonId, "Sleep Powder", "Hypnosis", "Spore", "Yawn", "Sing") then
+            sleeper = pokemonId
         end
     end
-    return nil
 end
-
-if multipleCatch == true then
-	return synchro == getPokemonNumberOfSynch("Synchronize")
 end
-
-if advanceCatchOn == true then
-	
-	return falseswiper == getPokemonNumberWithMove("False Swipe") and sleeper == getPokemonNumberWithMove(sleepMoves)
-
-	
-elseif fswipeOn == true then
-
-	return falseswiper == getPokemonNumberWithMove("False Swipe")
-	
-	
-elseif sleepOnOn == true then
-	
-	return sleeper == getPokemonNumberWithMove(sleepMoves)
-	
-	
-end
-
-for _,sleepMoves in pairs(sleepMoves) do
-  sleeMoves = sleepMoves
-    break
-end
- 
